3d571cc652433114e5250b2baf5a0ba346c99d86
Avery King <avery.n.king@wmich.edu>
2021-12-18 10:27:43 -0500

    update to checkInUse

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -12,14 +12,43 @@ const create = (newUser: object): Promise<object> => {
     return request.then(response => response.data);
 }
 
+const getAllUsernames = () => {
+    const request = axios.get(`${baseUrl}/usernames`);
+    return request.then(response => response.data);
+}
 
-const checkInUse = (values: { email?: string, username?: string }): Promise<boolean> => {
+const getAllEmails = () => {
+    const request = axios.get(`${baseUrl}/emails`);
+    return request.then(response => response.data)
+}
+const checkInUse = async (values: { email?: string, username?: string }, namesList?: string[], emailList?: string[]): Promise<boolean> => {
+    if(values.hasOwnProperty('email')) {
+        emailList?.forEach(email => {
+            if(email === values.email) {
+                return Promise.resolve(true);
+            } else {
+                return Promise.resolve(false);
+            }
+        })
+    } else {
+        namesList?.forEach(username => {
+            if(username === values.username) {
+                return Promise.resolve(true);
+            } else {
+                return Promise.resolve(false);
+            }
+        })
+    }
+
+    return Promise.resolve(false)
+    /** This implementation queries server-side API instead
+     *
     const checkObj = values.hasOwnProperty('email')
         ? {email: values.email}
         : {username: values.username}
     const request = axios.post(`${baseUrl}/checkInUse`, checkObj);
-    return request.then(response => response.data.inUse);
+    return request.then(response => response.data.inUse);*/
 }
 export default {
-    getAll, create, checkInUse
+    getAll, create, checkInUse, getAllUsernames, getAllEmails
 };
