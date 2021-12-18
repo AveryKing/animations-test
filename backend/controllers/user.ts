4b7b92368d7d7350092b127825de0fb46e6f1c95
Avery King <avery.n.king@wmich.edu>
2021-12-18 11:09:57 -0500

    getUserData API update

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -33,19 +33,49 @@ userRouter.post('/', async (req: Request, res: Response) => {
     return
 })
 
+userRouter.get('/getUserData', async (req: Request, res: Response) => {
+    User.find({})
+        .then(users => {
+            const userData = [];
+            const emailArray: Array<string> = [];
+            const userArray: Array<string> = [];
+            users.forEach(user => {
+
+                emailArray.push(user.email);
+                userArray.push(user.username);
+            })
+            userData.push(emailArray);
+            userData.push(userArray);
+
+            res.json({
+                userData:userData
+            });
+        })
+
+})
+
+
 /** Used to check if an email or username is already in use **/
 userRouter.post('/checkInUse', async (req: Request, res: Response) => {
+
     if (!req.body.email && !req.body.username) {
         res.json({
             error: 'The parameters provided are incorrect.'
         });
     }
 
-    const user = req.body.email
-        ? await User.findOne({email: req.body.email})
-        : await User.findOne({username: req.body.username})
 
-    res.json({
+    let user: IUser = req.body.email
+        ? await User.findOne({email: req.body.email})
+        : await User.findOne({username: req.body.username});
+    /*
+        //This block enforces exact match for emails.
+        if(user && req.body.email) {
+            user = user.email === req.body.email ? user : null
+        }*/
+
+    console.log(user)
+    return res.json({
         inUse: user ? true : false
     });
 })
