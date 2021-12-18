85aa00998794b1ec0fa0db7f56c57b455d8532e4
Avery King <avery.n.king@wmich.edu>
2021-12-18 11:09:16 -0500

    final checkInUse update

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -12,43 +12,28 @@ const create = (newUser: object): Promise<object> => {
     return request.then(response => response.data);
 }
 
-const getAllUsernames = () => {
-    const request = axios.get(`${baseUrl}/usernames`);
+const getUserData = () => {
+    const request = axios.get(`${baseUrl}/getUserData`);
     return request.then(response => response.data);
 }
 
-const getAllEmails = () => {
-    const request = axios.get(`${baseUrl}/emails`);
-    return request.then(response => response.data)
-}
-const checkInUse = async (values: { email?: string, username?: string }, namesList?: string[], emailList?: string[]): Promise<boolean> => {
-    if(values.hasOwnProperty('email')) {
-        emailList?.forEach(email => {
-            if(email === values.email) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.resolve(false);
-            }
-        })
-    } else {
-        namesList?.forEach(username => {
-            if(username === values.username) {
-                return Promise.resolve(true);
-            } else {
-                return Promise.resolve(false);
-            }
-        })
-    }
 
+const checkInUse = async (userData: any, values: { email?: string, username?: string }): Promise<boolean> => {
+    if (values.hasOwnProperty('email')) {
+        if (userData['userData'][0].includes(values.email)) {
+            return Promise.resolve(true)
+        } else {
+            return Promise.resolve(false)
+        }
+    } else {
+        if (userData['userData'][1].includes(values.username)) {
+            return Promise.resolve(true)
+        } else {
+            return Promise.resolve(false)
+        }
+    }
     return Promise.resolve(false)
-    /** This implementation queries server-side API instead
-     *
-    const checkObj = values.hasOwnProperty('email')
-        ? {email: values.email}
-        : {username: values.username}
-    const request = axios.post(`${baseUrl}/checkInUse`, checkObj);
-    return request.then(response => response.data.inUse);*/
 }
 export default {
-    getAll, create, checkInUse, getAllUsernames, getAllEmails
+    getAll, create, checkInUse, getUserData
 };
