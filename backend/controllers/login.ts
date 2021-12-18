e22d6554680005ae332470cb9ad6b367e5503a01
Avery King <avery.n.king@wmich.edu>
2021-12-18 02:19:03 -0500

    login APi added to ```loginRouter```

--- backend/controllers/login.ts
+++ backend/controllers/login.ts
@@ -1,9 +1,38 @@
+import {Request, Response} from 'express';
+import { User } from '../models/User'
 const loginRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-import { Request, Response } from 'express';
+const SECRET = 'H(#(#Ji2mk34jruje9k2i3ij4ufnhv';
 
-loginRouter.post('/', async (_req:Request, res:Response) => {
-   res.send('login')
+loginRouter.post('/', async (req: Request, res: Response) => {
+    const body = req.body
+
+    const user = await User.findOne({username: body.username});
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(body.password, user.password);
+
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: 'The credentials provided are incorrect.'
+        })
+    }
+
+    const userForToken = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, SECRET);
+
+    return res.status(200)
+        .send({
+            token,
+            username: user.username,
+            userId: user._id
+        })
 
 })
 
