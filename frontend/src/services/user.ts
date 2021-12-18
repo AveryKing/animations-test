3f963c762e2d992feb0b742f4f70e33870eb38e0
Avery King <avery.n.king@wmich.edu>
2021-12-18 07:54:10 -0500

    checkInUse API implemented client-side

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -2,21 +2,24 @@ import axios from 'axios';
 
 const baseUrl: string = 'http://localhost:6969/api/users';
 
-const getAll = () => {
+const getAll = (): Promise<object> => {
     const request = axios.get(`${baseUrl}`);
     return request.then(response => response.data);
 }
 
-const create = (email: string, username: string, password: string) => {
-    const newUser: object = {
-        email: email,
-        username: username,
-        password: password
-    };
+const create = (newUser: object): Promise<object> => {
     const request = axios.post(`${baseUrl}`, newUser);
     return request.then(response => response.data);
 }
 
+
+const checkInUse = (values: { email?: string, username?: string }): Promise<boolean> => {
+    const checkObj = values.hasOwnProperty('email')
+        ? {email: values.email}
+        : {username: values.username}
+    const request = axios.post(`${baseUrl}/checkInUse`, checkObj);
+    return request.then(response => response.data.inUse);
+}
 export default {
-    getAll, create
+    getAll, create, checkInUse
 };
