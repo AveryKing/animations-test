8424bc58dd40214c54b308afae56ffbe9a05aad4
Avery King <avery.n.king@wmich.edu>
2021-12-18 01:56:49 -0500

    ```UserSchema``` updated to enforce unique email and username requirement

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,4 +1,9 @@
 import {model, Schema, Model, Document} from 'mongoose';
+const uniqueValidator = require('mongoose-unique-validator');
+
+//Change these to enforce unique email/username requirements
+const REQUIRE_UNIQUE_EMAIL: boolean = true;
+const REQUIRE_UNIQUE_USERNAME: boolean = true;
 
 interface IUser extends Document {
     email: string;
@@ -8,10 +13,15 @@ interface IUser extends Document {
 
 
 const UserSchema: Schema = new Schema({
-    email: { type: String, required: true },
-    username: { type: String, required: true },
+    email: { type: String, required: true, unique: REQUIRE_UNIQUE_EMAIL },
+    username: { type: String, required: true, unique: REQUIRE_UNIQUE_USERNAME },
     password: { type: String, required: true }
 });
+
+if(REQUIRE_UNIQUE_EMAIL || REQUIRE_UNIQUE_USERNAME) {
+    UserSchema.plugin(uniqueValidator);
+}
+
 
 const User: Model<IUser> = model('User', UserSchema)
 
